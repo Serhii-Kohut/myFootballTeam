da144e832cf704f7a81d25d0eb82bdaecdebf5f2
Serhii Kohut <113467605+ronycv@users.noreply.github.com>
2023-07-12 09:12:47 +0300

    Entity - realised pom - updated

---
+++ src/main/java/com/serhii/myproject/model/User.java
@@ -0,0 +1,44 @@
+package com.serhii.myproject.model;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import lombok.Data;
+
+import javax.management.relation.Role;
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+@Data
+public class User {
+    private static final String NAME_REGEXP = "[A-Z][a-z]+(-[A-Z][a-z]+){0,1}";
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Pattern(regexp = NAME_REGEXP,
+            message = "Must start with a capital letter followed by one or more lowercase letters")
+    @Column(name = "name", nullable = false)
+    private String firstname;
+
+    @Pattern(regexp = NAME_REGEXP,
+            message = "Must start with a capital letter followed by one or more lowercase letters")
+    @Column(name = "lastname", nullable = false)
+    private String lastname;
+
+    @Email(message = "Must be a valid e-mail address")
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?$\\.&]{8,}$",
+            message = "Must be minimum 8 characters, at least one uppercase, one lowercase, one number and one special character")
+    @Column(nullable = false)
+    private String password;
+
+    @ManyToOne
+    @JoinColumn(name = "roleid")
+    @NotNull
+    private Role role;
+}
