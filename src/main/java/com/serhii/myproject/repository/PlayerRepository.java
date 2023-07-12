b665b7be5c37c9a35429c06c8f4c04d810e0e248
Serhii Kohut <113467605+ronycv@users.noreply.github.com>
2023-07-12 20:40:17 +0300

    repository - added pom - updated

---
+++ src/main/java/com/serhii/myproject/repository/PlayerRepository.java
@@ -0,0 +1,9 @@
+package com.serhii.myproject.repository;
+
+import com.serhii.myproject.model.Player;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PlayerRepository extends JpaRepository<Player, Long> {
+}
