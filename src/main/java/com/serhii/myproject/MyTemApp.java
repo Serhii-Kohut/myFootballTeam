b4fea20a6f6ed965bfd2d331b9f06d755d5a71cc
Serhii Kohut <113467605+ronycv@users.noreply.github.com>
2023-07-15 16:59:14 +0300

    added MyTemApp.java
    pom - added dependencies Spring boot starter web

---
+++ src/main/java/com/serhii/myproject/MyTemApp.java
@@ -0,0 +1,13 @@
+package com.serhii.myproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+
+@SpringBootApplication
+public class MyTemApp extends SpringBootServletInitializer {
+    public static void main(String[] args) {
+        SpringApplication.run(MyTemApp.class);
+    }
+}
