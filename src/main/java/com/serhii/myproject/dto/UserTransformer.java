26be657ec8d1f5cf05a7bebefa028f76814eec97
Serhii Kohut <113467605+ronycv@users.noreply.github.com>
2023-07-16 20:39:48 +0300

    updated UserTransformer, create-user.html, user-list

--- src/main/java/com/serhii/myproject/dto/UserTransformer.java
+++ src/main/java/com/serhii/myproject/dto/UserTransformer.java
@@ -1,7 +1,7 @@
 package com.serhii.myproject.dto;
 
+import com.serhii.myproject.model.Role;
 import com.serhii.myproject.model.User;
-import com.serhii.myproject.service.RoleService;
 
 public class UserTransformer {
     public static UserDto convertToDto(User user) {
@@ -11,18 +11,18 @@ public class UserTransformer {
                 user.getLastName(),
                 user.getEmail(),
                 user.getPassword(),
-                user.getRole().getName()
+                user.getRole()
         );
     }
 
-    public static User convertToEntity(UserDto userDto, RoleService roleService) {
+    public static User convertToEntity(UserDto userDto) {
         User user = new User();
         user.setId(userDto.getId());
         user.setFirstName(userDto.getFirstName());
         user.setLastName(userDto.getLastName());
         user.setEmail(userDto.getEmail());
         user.setPassword(userDto.getPassword());
-        user.setRole(roleService.readByName(userDto.getRole()));
+        user.setRole(userDto.getRole());
         return user;
     }
 
