b63bf73edbcffd1ecf660ad49722569ae4cbaddb
Serhii Kohut <113467605+ronycv@users.noreply.github.com>
2023-07-18 08:31:41 +0300

    class Player - minor update

---
+++ src/main/java/com/serhii/myproject/controller/PlayerController.java
@@ -0,0 +1,10 @@
+package com.serhii.myproject.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/players")
+public class PlayerController {
+
+}
