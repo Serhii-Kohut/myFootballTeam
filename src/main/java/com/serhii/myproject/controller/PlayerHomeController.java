2d848b54c360a9e52871ba40198932705e1c467d
Serhii Kohut <113467605+ronycv@users.noreply.github.com>
2023-07-20 09:56:41 +0300

    PlayerHomeController.java - realised

---
+++ src/main/java/com/serhii/myproject/controller/PlayerHomeController.java
@@ -0,0 +1,30 @@
+package com.serhii.myproject.controller;
+
+import com.serhii.myproject.dto.PlayerDto;
+import com.serhii.myproject.dto.PlayerTransformer;
+import com.serhii.myproject.service.PlayerService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Controller
+public class PlayerHomeController {
+    private final PlayerService playerService;
+
+    public PlayerHomeController(PlayerService playerService) {
+        this.playerService = playerService;
+    }
+
+    @GetMapping("/player-home")
+    public String showPlayerHome(Model model) {
+        List<PlayerDto> playerDtos = playerService.getAllPlayers().stream()
+                .map(PlayerTransformer::convertToDto)
+                .collect(Collectors.toList());
+
+        model.addAttribute("players", playerDtos);
+        return "player-home";
+    }
+}
