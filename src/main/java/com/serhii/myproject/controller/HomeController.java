4d7dd19d02821dd07222d16baa3dff1b21c8e570
Serhii Kohut <113467605+ronycv@users.noreply.github.com>
2023-07-22 15:07:07 +0300

    HomeController, home-page.html - update
    added choice-card.css

--- src/main/java/com/serhii/myproject/controller/HomeController.java
+++ src/main/java/com/serhii/myproject/controller/HomeController.java
@@ -1,10 +1,12 @@
 package com.serhii.myproject.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+    @GetMapping({"/", "/home"})
     public String showHomePage(){
-        return "home";
+        return "home-page";
     }
 }
