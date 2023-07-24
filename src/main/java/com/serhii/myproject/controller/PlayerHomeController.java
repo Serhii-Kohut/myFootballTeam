c08a343657bce3d770404681e0b5341a8d528045
Serhii Kohut <113467605+ronycv@users.noreply.github.com>
2023-07-24 08:53:02 +0300

    implemented methods of displaying all positions separately

--- src/main/java/com/serhii/myproject/controller/PlayerHomeController.java
+++ src/main/java/com/serhii/myproject/controller/PlayerHomeController.java
@@ -2,6 +2,7 @@ package com.serhii.myproject.controller;
 
 import com.serhii.myproject.dto.PlayerDto;
 import com.serhii.myproject.dto.PlayerTransformer;
+import com.serhii.myproject.model.PlayerPosition;
 import com.serhii.myproject.service.PlayerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -19,13 +20,61 @@ public class PlayerHomeController {
         this.playerService = playerService;
     }
 
-    @GetMapping("/player-home")
-    public String showPlayerHome(Model model) {
-        List<PlayerDto> playerDtos = playerService.getAllPlayers().stream()
-                .map(PlayerTransformer::convertToDto).sorted(Comparator.comparingLong(PlayerDto::getId))
+    /*    @GetMapping("/player-home")
+        public String showPlayerHome(Model model) {
+            List<PlayerDto> playerDtos = playerService.getAllPlayers().stream()
+                    .map(PlayerTransformer::convertToDto).sorted(Comparator.comparingLong(PlayerDto::getId))
+                    .collect(Collectors.toList());
+
+            model.addAttribute("players", playerDtos);
+            return "player-home";
+        }*/
+    @GetMapping("/player-home/goalkeepers")
+    public String showGoalkeepersPlayers(Model model) {
+        List<PlayerDto> goalkeepersPlayerDtos = playerService.getByPosition(PlayerPosition.valueOf("Goalkeeper"))
+                .stream()
+                .map(PlayerTransformer::convertToDto)
+                .sorted(Comparator.comparing(PlayerDto::getId))
                 .collect(Collectors.toList());
 
-        model.addAttribute("players", playerDtos);
-        return "player-home";
+        model.addAttribute("players", goalkeepersPlayerDtos);
+        return "player-list";
     }
+
+    @GetMapping("/player-home/defenders")
+    public String showDefendersPlayers(Model model) {
+        List<PlayerDto> defendersPlayerDtos = playerService.getByPosition(PlayerPosition.valueOf("Defender"))
+                .stream()
+                .map(PlayerTransformer::convertToDto)
+                .sorted(Comparator.comparing(PlayerDto::getId))
+                .collect(Collectors.toList());
+
+        model.addAttribute("players", defendersPlayerDtos);
+        return "player-list";
+    }
+
+    @GetMapping("/player-home/midfielders")
+    public String showMidfieldersPlayers(Model model) {
+        List<PlayerDto> midfieldersPlayerDtos = playerService.getByPosition(PlayerPosition.valueOf("Midfielder"))
+                .stream()
+                .map(PlayerTransformer::convertToDto)
+                .sorted(Comparator.comparing(PlayerDto::getId))
+                .collect(Collectors.toList());
+
+        model.addAttribute("players", midfieldersPlayerDtos);
+        return "player-list";
+    }
+
+    @GetMapping("/player-home/forwards")
+    public String showForwardsPlayers(Model model) {
+        List<PlayerDto> forwardsPlayerDtos = playerService.getByPosition(PlayerPosition.valueOf("Forward"))
+                .stream()
+                .map(PlayerTransformer::convertToDto)
+                .sorted(Comparator.comparing(PlayerDto::getId))
+                .collect(Collectors.toList());
+
+        model.addAttribute("players", forwardsPlayerDtos);
+        return "player-list";
+    }
+
 }
