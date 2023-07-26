b794c27d64990b86619dbd8a588c6c9cc7ec5f75
Serhii Kohut <113467605+ronycv@users.noreply.github.com>
2023-07-26 22:15:13 +0300

    implements CustomUserDetails

---
+++ src/main/java/com/serhii/myproject/service/impl/CustomUserDetails.java
@@ -0,0 +1,30 @@
+package com.serhii.myproject.service.impl;
+
+import com.serhii.myproject.model.User;
+import com.serhii.myproject.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.Collections;
+
+public class CustomUserDetails implements UserDetailsService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("Username wasn't found");
+        }
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getEmail(),
+                user.getPassword(),
+                Collections.singletonList(new SimpleGrantedAuthority(user.getRole().name()))
+        );
+    }
+}
