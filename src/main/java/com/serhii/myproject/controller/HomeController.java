5b7ae1d0a23e9f76df0f992a62a9006a280a58db
Serhii Kohut <113467605+ronycv@users.noreply.github.com>
2023-07-28 22:34:25 +0300

    HomeController.java, header.html - attempt to add greatings

--- src/main/java/com/serhii/myproject/controller/HomeController.java
+++ src/main/java/com/serhii/myproject/controller/HomeController.java
@@ -1,12 +1,38 @@
 package com.serhii.myproject.controller;
 
+import com.serhii.myproject.model.User;
+import com.serhii.myproject.repository.UserRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.security.Principal;
 
 @Controller
 public class HomeController {
+
+    private final UserRepository userRepository;
+
+    public HomeController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     @GetMapping({"/", "/home"})
-    public String showHomePage(){
+    public String homePage(Model model, Principal principal) {
+        User userDto = null;
+        if (principal != null) {
+            String[] fullName = principal.getName().split(" ");
+            String firstName = fullName[0];
+            String lastName = fullName.length > 1 ? fullName[1] : "";
+
+            userDto = userRepository.findByEmail(principal.getName());
+
+            model.addAttribute("firstName", firstName);
+            model.addAttribute(
+                    "lastName", lastName);
+        }
+        model.addAttribute("user", userDto);
         return "home-page";
     }
 }
+
