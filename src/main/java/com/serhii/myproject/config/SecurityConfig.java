5de38b3ddc5d7ab3102af6d5a7ade240d0dc5a50
Serhii Kohut <113467605+ronycv@users.noreply.github.com>
2023-07-29 16:09:29 +0300

    pom.xml - added thymeleaf-extras-springsecurity5
    SecurityConfig.java - added springSecurityDialect

--- src/main/java/com/serhii/myproject/config/SecurityConfig.java
+++ src/main/java/com/serhii/myproject/config/SecurityConfig.java
@@ -3,12 +3,14 @@ package com.serhii.myproject.config;
 import com.serhii.myproject.model.Role;
 import com.serhii.myproject.service.impl.CustomUserDetails;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.thymeleaf.extras.springsecurity5.dialect.SpringSecurityDialect;
 
 @Configuration
 @EnableGlobalMethodSecurity(jsr250Enabled = true)
@@ -28,10 +30,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.authorizeRequests()
                 .antMatchers("/public/**").permitAll()
                 .antMatchers("/users/**").hasAuthority("PRESIDENT")
-                .antMatchers("/players/**").hasAnyAuthority("PRESIDENT", "SPORT_DIRECTOR")
+                .antMatchers("/players/**").hasAnyAuthority("PRESIDENT", "SPORT_DIRECTOR", "COACH")
                 .anyRequest().authenticated()
                 .and()
                 .formLogin();
+    }
+
+    @Bean
+    public SpringSecurityDialect springSecurityDialect() {
+        return new SpringSecurityDialect();
     }
 }
 
