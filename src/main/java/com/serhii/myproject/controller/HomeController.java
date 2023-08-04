3847a87bfefbcf7f3b13e64bb8ced324742be0b4
Serhii Kohut <113467605+ronycv@users.noreply.github.com>
2023-08-04 21:27:25 +0300

    HomeController.java LogInController.java - added logger

--- src/main/java/com/serhii/myproject/controller/HomeController.java
+++ src/main/java/com/serhii/myproject/controller/HomeController.java
@@ -2,6 +2,8 @@ package com.serhii.myproject.controller;
 
 import com.serhii.myproject.model.User;
 import com.serhii.myproject.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +12,7 @@ import java.security.Principal;
 
 @Controller
 public class HomeController {
+    private Logger logger = LoggerFactory.getLogger(HomeController.class);
 
     private final UserRepository userRepository;
 
@@ -19,11 +22,15 @@ public class HomeController {
 
     @GetMapping({"/", "/home"})
     public String homePage(Model model, Principal principal) {
+
         User userDto = null;
         if (principal != null) {
             userDto = userRepository.findByEmail(principal.getName());
             model.addAttribute("user", userDto);
         }
+
+        logger.info("Home page was showed");
+
         return "home-page";
     }
 }
--- src/main/java/com/serhii/myproject/controller/LogInController.java
+++ src/main/java/com/serhii/myproject/controller/LogInController.java
@@ -1,5 +1,7 @@
 package com.serhii.myproject.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,8 +9,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/custom-login")
 public class LogInController {
+
+    Logger logger = LoggerFactory.getLogger(LogInController.class);
     @GetMapping
     public String login() {
+
+        logger.info("Custom login page was showed");
+
         return "customLogin";
     }
 
