d895327f3678e9b28e6d3fbfe2b2ea917ca37146
Serhii Kohut <113467605+ronycv@users.noreply.github.com>
2023-08-05 13:27:05 +0300

    SecurityConfig.java - changed annotation EnableGlobalMethodSecurity: added prePostEnabled

--- src/main/java/com/serhii/myproject/config/SecurityConfig.java
+++ src/main/java/com/serhii/myproject/config/SecurityConfig.java
@@ -13,7 +13,7 @@ import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.thymeleaf.extras.springsecurity5.dialect.SpringSecurityDialect;
 
 @Configuration
-@EnableGlobalMethodSecurity(jsr250Enabled = true)
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private CustomUserDetails customUserDetails;
