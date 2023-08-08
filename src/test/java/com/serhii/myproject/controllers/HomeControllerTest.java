e0c8e1281c6fd677800bbabbbb4f2e6766238fa3
Serhii Kohut <113467605+ronycv@users.noreply.github.com>
2023-08-08 09:06:01 +0300

    HomeControllerTest.java - updated testHomePageWhenAuthenticated and added testHomePageWhenNotAuthenticated
    pom.xml - added dependence spring-security-test

--- src/test/java/com/serhii/myproject/controllers/HomeControllerTest.java
+++ src/test/java/com/serhii/myproject/controllers/HomeControllerTest.java
@@ -1,24 +1,28 @@
 package com.serhii.myproject.controllers;
 
+import com.serhii.myproject.config.SecurityConfig;
+import com.serhii.myproject.controller.HomeController;
 import com.serhii.myproject.model.User;
 import com.serhii.myproject.repository.UserRepository;
+import com.serhii.myproject.service.impl.CustomUserDetails;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
-
-import java.security.Principal;
-
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
-@AutoConfigureMockMvc
+@RunWith(SpringRunner.class)
+@WebMvcTest(HomeController.class)
+@ContextConfiguration(classes = {HomeController.class, SecurityConfig.class})
 public class HomeControllerTest {
 
     @Autowired
@@ -27,23 +31,27 @@ public class HomeControllerTest {
     @MockBean
     private UserRepository userRepository;
 
-    @Test
-    public void testHomePage() throws Exception {
-        User user = new User();
-        user.setEmail("test@test.com");
-        when(userRepository.findByEmail("test@test.com")).thenReturn(user);
+    @MockBean
+    private CustomUserDetails customUserDetails;
 
-        mockMvc.perform(get("/home").principal(new Principal() {
-                    @Override
-                    public String getName() {
-                        return "test@test.com";
-                    }
-                }))
+    @Test
+    @WithMockUser(username = "test@example.com")
+    public void testHomePageWhenAuthenticated() throws Exception {
+        User user = new User();
+        user.setEmail("test@example.com");
+        when(userRepository.findByEmail("test@example.com")).thenReturn(user);
+
+        mockMvc.perform(get("/home").principal(() -> "test@example.com"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("home-page"))
-                .andExpect(model().attributeExists("user"))
                 .andExpect(model().attribute("user", user));
-
     }
 
+    @Test
+    public void testHomePageWhenNotAuthenticated() throws Exception {
+        mockMvc.perform(get("/home"))
+                .andExpect(status().isFound())
+                .andExpect(redirectedUrl("http://localhost/custom-login"));
+    }
 }
+
