301ea4c058389ff9acbba84078e0968dae8fe639
Serhii Kohut <113467605+ronycv@users.noreply.github.com>
2023-08-16 21:52:51 +0300

    UserServiceTest - successfully realised testReadUser

--- src/test/java/com/serhii/myproject/services/UserServiceTest.java
+++ src/test/java/com/serhii/myproject/services/UserServiceTest.java
@@ -11,8 +11,6 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.transaction.Transactional;
 
-import org.junit.Assert;
-
 import static org.hibernate.validator.internal.util.Contracts.assertNotNull;
 import static org.junit.Assert.assertEquals;
 
@@ -38,6 +36,26 @@ public class UserServiceTest {
         assertNotNull(createdUser.getId());
         assertEquals("John", createdUser.getFirstName());
         assertEquals(Role.SPORT_DIRECTOR, createdUser.getRole());
+
+    }
+
+    @Test
+    public void testReadUser() {
+        User user = new User();
+        user.setFirstName("John");
+        user.setLastName("Elkann");
+        user.setEmail("test@emample.com");
+        user.setPassword("51515Sdsd");
+        user.setRole(Role.valueOf(Role.SPORT_DIRECTOR.name()));
+
+        User createdUser = userService.create(user);
+
+        User userById = userService.readById(createdUser.getId());
+
+        assertNotNull(userById.getId());
+        assertEquals("John", userById.getFirstName());
+        assertEquals("Elkann", userById.getLastName());
+        assertEquals(Role.SPORT_DIRECTOR, userById.getRole());
 
     }
 }
