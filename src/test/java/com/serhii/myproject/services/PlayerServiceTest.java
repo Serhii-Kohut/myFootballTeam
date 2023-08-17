25ff9ce383041f2e1b3dbd8c284ecac0bbb9258f
Serhii Kohut <113467605+ronycv@users.noreply.github.com>
2023-08-17 21:31:05 +0300

    PlayerServiceTest - added and successfully realised setUp method  with annotation @Before and testCreatePlayer

---
+++ src/test/java/com/serhii/myproject/services/PlayerServiceTest.java
@@ -0,0 +1,54 @@
+package com.serhii.myproject.services;
+
+import com.serhii.myproject.model.Player;
+import com.serhii.myproject.model.PlayerPosition;
+import com.serhii.myproject.service.PlayerService;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.transaction.Transactional;
+import java.time.LocalDate;
+import java.util.Optional;
+
+import static org.hibernate.validator.internal.util.Contracts.assertNotNull;
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@Transactional
+@SpringBootTest
+public class PlayerServiceTest {
+    @Autowired
+    private PlayerService playerService;
+
+    private Player player1;
+    private Player player2;
+
+    @Before
+    public void setUp() {
+        player1 = new Player();
+        player1.setPlayerFirstName("Cris");
+        player1.setPlayerLastName("Pulisic");
+        player1.setPosition(PlayerPosition.MIDFIELDER);
+        player1.setDateOfBirth(LocalDate.of(1999, 6, 11));
+        player1.setJerseyNumber(17L);
+        player1.setMarketValue(25000000L);
+
+        playerService.create(player1);
+
+    }
+
+    @Test
+    public void testCreatePlayer() {
+        Player createdPlayer = playerService.create(player1);
+
+        assertNotNull(createdPlayer.getId());
+        assertEquals("Cris", createdPlayer.getPlayerFirstName());
+        assertEquals(Optional.of(25000000L), Optional.ofNullable(createdPlayer.getMarketValue()));
+        assertEquals(PlayerPosition.MIDFIELDER, createdPlayer.getPosition());
+    }
+
+}
