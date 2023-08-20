f7916cb3730822cdee3ef10c34a8a1b6f49ad161
Serhii Kohut <113467605+ronycv@users.noreply.github.com>
2023-08-20 22:48:22 +0300

    AwsConfig.java - update Value

--- src/main/java/com/serhii/myproject/config/AwsConfig.java
+++ src/main/java/com/serhii/myproject/config/AwsConfig.java
@@ -11,13 +11,13 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AwsConfig {
-    @Value("${cloud.aws.credentials.accessKey}")
+    @Value("${spring.cloud.aws.s3.credentials.access-key}")
     private String accessKey;
 
-    @Value("${cloud.aws.credentials.secretKey}")
+    @Value("${spring.cloud.aws.s3.credentials.secret-key}")
     private String secretKey;
 
-    @Value("${cloud.aws.region.static}")
+    @Value("${spring.cloud.aws.s3.region.static}")
     private String region;
 
     @Bean
