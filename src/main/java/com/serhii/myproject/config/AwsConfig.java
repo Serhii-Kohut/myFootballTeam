f6604351ebc2eba7190823871dc07bc4d8d00ea1
Serhii Kohut <113467605+ronycv@users.noreply.github.com>
2023-08-23 09:02:44 +0300

    AwsConfig - added method generatePresignedUrl and field bucketName

--- src/main/java/com/serhii/myproject/config/AwsConfig.java
+++ src/main/java/com/serhii/myproject/config/AwsConfig.java
@@ -1,15 +1,22 @@
 package com.serhii.myproject.config;
 
+import com.amazonaws.HttpMethod;
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.GeneratePresignedUrlRequest;
+import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.net.URL;
+import java.util.Date;
+
 @Configuration
+@Getter
 public class AwsConfig {
     @Value("${spring.cloud.aws.s3.credentials.access-key}")
     private String accessKey;
@@ -20,6 +27,9 @@ public class AwsConfig {
     @Value("${spring.cloud.aws.s3.region.static}")
     private String region;
 
+    @Value("${spring.cloud.aws.s3.bucket}")
+    private String bucketName;
+
     @Bean
     public AmazonS3 amazonS3() {
         AWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
@@ -27,6 +37,21 @@ public class AwsConfig {
                 .withCredentials(new AWSStaticCredentialsProvider(credentials))
                 .withRegion(region)
                 .build();
+    }
+
+    public URL generatePresignedUrl(String bucketName, String objectKey) {
+        AmazonS3 s3client = amazonS3();
+
+        Date expiration = new Date();
+        long expTimeMillis = expiration.getTime();
+        expTimeMillis += 1000 * 60 * 60; // 1 hour
+        expiration.setTime(expTimeMillis);
+
+        GeneratePresignedUrlRequest generatePresignedUrlRequest =
+                new GeneratePresignedUrlRequest(bucketName, objectKey)
+                        .withMethod(HttpMethod.GET)
+                        .withExpiration(expiration);
+        return s3client.generatePresignedUrl(generatePresignedUrlRequest);
     }
 
 }
